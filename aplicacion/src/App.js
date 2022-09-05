9b373f481a47a362ee110f5b7571e3b15ac30f12
luana <luanaesteve@hotmail.com>
2022-09-05 09:59:50 -0300

    dasda

--- aplicacion/src/App.js
+++ aplicacion/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactddaaa
+          Learn React
         </a>
       </header>
     </div>
