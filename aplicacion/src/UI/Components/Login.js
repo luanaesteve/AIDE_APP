2f124ca9e5ca96167101da62309ebbd5d7bd3f29
luana <luanaesteve@hotmail.com>
2022-09-05 15:47:00 -0300

    jaja

--- aplicacion/src/UI/Components/Login.js
+++ aplicacion/src/UI/Components/Login.js
@@ -36,8 +36,8 @@ export const Login = () => {
         <div>
           <h1 className="h11">Ingresar</h1>
           <Form>
-            <TextField label="Email" name="email" type="email" />
-            <TextField label="password" name="password" type="password" />
+            <TextField label="Email" name="email" type="email" placeholder="Email" />
+            <TextField label="password" name="password" type="password" placeholder="Contraseña" />
             <button className="boton" type="submit">Login</button>
           </Form>
         </div>
