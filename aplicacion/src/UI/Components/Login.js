efcf571577ca0120b98b4a349ed06b9845086f2f
luana <luanaesteve73@gmail.com>
2022-09-05 22:12:26 -0300

    hjadshdajdhhj

--- aplicacion/src/UI/Components/Login.js
+++ aplicacion/src/UI/Components/Login.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
 import { TextField } from './TextField';
-import * as Yup from 'yup';
 
 export const Login = () => {
+  /*
   const validate = Yup.object({
     email: Yup.string()
       .email('El email es invalido')
@@ -11,17 +11,18 @@ export const Login = () => {
     password: Yup.string()
       .min(6, 'La contraseña debe tener al menos 6 caracteres.')
       .required('Contraseña requerida'),
-  })
+  })*/
   return (
     <Formik
       initialValues={{
         email: '',
         password: '',
       }}
+      /*
       validationSchema={validate}
       onSubmit={values => {
         console.log(values)
-      }}
+      }}*/
     >
       {formik => (
         <div>
@@ -29,7 +30,7 @@ export const Login = () => {
           <Form>
             <TextField name="email" type="email" placeholder="Email" />
             <TextField name="password" type="password" placeholder="Contraseña" />
-            <button id='submit' className="boton" type="submit">Login</button>
+            <button onClick="" id='submit' className="boton" type="submit">Login</button>
           </Form>
         </div>
       )}
