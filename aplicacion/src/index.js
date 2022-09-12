2c396bc4d4ec3204b5ec9216342f910afaff6ff0
abi <salcedo.abigail.epet20@gmail.com>
2022-09-12 16:09:52 -0300

    ASASASASAS

--- aplicacion/src/index.js
+++ aplicacion/src/index.js
@@ -1,19 +1,12 @@
-import react from 'react';
-import {View, Text} from 'react-native';
-import {ScreenHome} from './UI/Components/HomeScreen';
-import {BottomNavigation} from './UI/Components/Bottom';
-import { NavigationContainer } from '@react-navigation/bottom-tabs';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Stack = createNativeStackNavigator();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-export const Navigation = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName='welcome'>
-              <h1>skjajakjajkjkaakj</h1>
-                <Stack.Screen name="welcome"component={WelcomeScreen} options={{ headerShown: false }} />
-                <Stack.Screen name="welcome"component={WelcomeScreen} options={{ title: ', headerShown: false' }} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    ) }
