e1c8695afa48210ce9d435dfb46f11460517cb9f
luana <luanaesteve@hotmail.com>
2022-09-12 16:11:12 -0300

    dasd

---
+++ aplicacion/src/UI/Components/Navigation/index.js
@@ -0,0 +1,20 @@
+import react from 'react';
+import {View, Text} from 'react-native';
+import {ScreenHome} from './UI/Components/HomeScreen';
+import {BottomNavigation} from './UI/Components/Bottom';
+import { NavigationContainer } from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export const Navigation = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='welcome'>
+              <h1>skjajakjajkjkaakj</h1>
+                <Stack.Screen name="welcome"component={WelcomeScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="welcome"component={WelcomeScreen} options={{ title: ', headerShown: false' }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
