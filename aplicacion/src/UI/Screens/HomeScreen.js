a7da69879ace2fa254d8a1a96a6e1db85536c7a5
luana <luanaesteve@hotmail.com>
2022-09-13 15:05:02 -0300

    fdsfs

---
+++ aplicacion/src/UI/Screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text 
+            style={{
+                fontSize: 30,
+                textAlign: "center",
+                marginTop: "20%"
+            }}
+
+
+            >Home!</Text>
+        </View>
+    );
+}
+
+export default HomeScreen;
---
+++ aplicacion/src/UI/Screens/LoginScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
+
+const LoginScreen = () => {
+    return (
+        <View>
+            <Text 
+            style={{
+                fontSize: 30,
+                textAlign: "center",
+                marginTop: "20%"
+            }}
+
+
+            >Login!</Text>
+        </View>
+    );
+}
+
+export default LoginScreen;
