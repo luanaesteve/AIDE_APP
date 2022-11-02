92a5342fbe7bea34c17102aa814bbcf772458fe4
luana <luanaesteve73@gmail.com>
2022-11-02 16:22:02 -0300

    cxzkjcnxkv

--- src/UI/Screens/LoginScreen.js
+++ src/UI/Screens/LoginScreen.js
@@ -1,13 +1,2 @@
-import React from 'react';
-import { TextField } from '../Components/TextField';
-import Superiorlogin from '../Image/Superiorlogin.png';
-import linea1 from '../Image/linea1.png';
-import linea2 from '../Image/linea2.png';
-import google from '../Image/google.png';
-import facebook from '../Image/facebook.png';
-import tw from '../Image/tw.png';
-
-
 export default function LoginScreen() {
-   
 }
