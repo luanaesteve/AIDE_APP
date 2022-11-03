8cb96488fd0d98296e7b99901fea7e0dfaadba53
luana <luanaesteve73@gmail.com>
2022-11-03 11:17:58 -0300

    nfcjdnfksknf

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Route, Routes } from "react-router-dom";
 import LoginScreen from '../src/UI/Screens/LoginScreen';
 import RegisterScreen from '../src/UI/Screens/RegisterScreen';
 import HomeScreen from '../src/UI/Screens/HomeScreen';
-import {AuthProvider} from './UI/Context/authContext';
+import {AuthProvider} from './UI/Context/AuthContext';
+import { ProtectedRoute } from '../src/UI/Screens/ProtectedRoute'
 
 function App(){
 return(
@@ -12,7 +13,7 @@ return(
   <Routes>
     <Route path='/' element={<LoginScreen/>} />
     <Route path='/register' element={<RegisterScreen/>} />
-    <Route path='/home' element={<HomeScreen/>} />
+    <Route path='/home' element={<ProtectedRoute><HomeScreen/></ProtectedRoute>} />
     </Routes>
     </AuthProvider>
 );
