82652264e397ff9ba255472afc0bda8697420d88
luana <luanaesteve73@gmail.com>
2022-11-06 20:03:29 -0300

    asndaskdna

--- src/UI/Screens/HomeScreen.js
+++ src/UI/Screens/HomeScreen.js
@@ -13,11 +13,11 @@ export default function HomeScreen() {
   };
 
   return (
-    <div className="w-full max-w-xs m-auto text-black">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
       <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <p className="text-xl mb-4">welcome {user.displayName || user.email}</p>
         <button
-          className="bg-slate-200 hover:bg-slate-300 rounded py-2 px-4 text-black"
+          className=" text-center bg-slate-200 hover:bg-slate-300 rounded py-2 px-4 text-black"
           onClick={handleLogout}
         >
           Cerrar sesion 
