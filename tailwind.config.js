e428d9ab1b4aa13dfc25cc96e2c01b0bc926fc0b
luana <luanaesteve73@gmail.com>
2022-11-06 20:03:51 -0300

    colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,36 @@ module.exports = {
 
       'desktop': '1280px',
       // => @media (min-width: 1280px) { ... }
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+    
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+    
+      'lg': '992px',
+      // => @media (min-width: 992px) { ... }
+    
+      'xl': '1200px',
+      // => @media (min-width: 1200px) { ... }
     },
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#42275A',
+        'secondary': '#734B6D',
+        'violetita': '#B59FD7',
+        'gris': '#D9D9D9',
+        'violetaosc':'#230631',
+        'blue': '#1fb6ff',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'black': '#000000',
+      'white': '#FFFFFF'
+      }
+    },
   },
   plugins: [],
 }
