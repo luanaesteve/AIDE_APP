fb7433af42a35b50cb38f1c8b68a850e131e78e4
luana <luanaesteve73@gmail.com>
2022-11-11 14:00:17 -0300

    jajaj

--- src/UI/Screens/HomeScreen.js
+++ src/UI/Screens/HomeScreen.js
@@ -1,4 +1,6 @@
 import { useAuth } from "../Context/AuthContext";
+import { RiHomeHeartFill } from "react-icons/ri";
+import logo from "../Image/logo.png"
 
 export default function HomeScreen() {
   const { logout, user } = useAuth();
@@ -13,17 +15,37 @@ export default function HomeScreen() {
   };
 
   return (
-    <div className="min-h-screen grid grid-cols-6">
-      <div className="bg-green-300 col-span-1">
-        {/*Logotipo*/}
-        <div className="text-center p-8">
-          <h1 className="uppercase font-bold tracking-[4px]">TU LOGO</h1>
+    <div className=" min-h-screen grid grid-cols-6 overflow-y-scroll">
+      <div className=" bg-violetaosc p-10  flex flex-col justify-center gap-2 items-center">
+        <div className="w-20 h-20 items-center">
+        <img src={logo} className=""></img>
           </div>
         {/* MENU */}
         <nav>
           <ul>
             <li>
-              
+              <a href="#" className="flex items-center p-8 gap-2 text-gray p-4 hover:bg-purple-600 hover:text-white 
+              rounded-lg transition-colors font-semibold">
+              <RiHomeHeartFill/>Inicio
+              </a>
+            </li>
+            <li>
+              <a href="#" className=" flex items-center p-8 gap-2 text-gray p-4 hover:bg-purple-600 hover:text-white 
+              rounded-lg transition-colors font-semibold">
+              <RiHomeHeartFill/>Inicio
+              </a>
+            </li>
+            <li>
+              <a href="#" className=" flex items-center p-8 gap-2 text-gray p-4 hover:bg-purple-600 hover:text-white 
+              rounded-lg transition-colors font-semibold">
+              <RiHomeHeartFill/>Inicio
+              </a>
+            </li>
+            <li>
+              <a href="#" className=" flex items-center p-8 gap-2 text-gray p-4 hover:bg-purple-600 hover:text-white 
+              rounded-lg transition-colors font-semibold">
+              <RiHomeHeartFill/>Inicio
+              </a>
             </li>
           </ul>
         </nav>
