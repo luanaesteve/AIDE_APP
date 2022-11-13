cf74224ffb4e04437f4e79b2e459b0cc3d21ca59
luana <luanaesteve73@gmail.com>
2022-11-12 23:32:19 -0300

    interfaz

--- src/UI/Screens/HomeScreen.js
+++ src/UI/Screens/HomeScreen.js
@@ -15,85 +15,91 @@ export default function HomeScreen() {
       <Header />
       {/* Section 1 */}
       <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 mt-10 gap-8">
-        {/* Card 1 */}
-        <div className="bg-primary-100 p-8 rounded-xl text-gray-300 flex flex-col gap-6">
-          <RiLineChartLine className="text-5xl" />
-          <h4 className="text-2xl">Earnings</h4>
-          <span className="text-5xl text-white">&euro; 8,350</span>
-          <span className="py-1 px-3 bg-primary-300/80 rounded-full">
-            + 10% since last month
-          </span>
-        </div>
-        {/* Card 2 */}
-        <div className="p-4 bg-white rounded-xl flex flex-col justify-between gap-4 drop-shadow-2xl">
-          <div className="flex items-center gap-4 bg-primary-100/10 rounded-xl p-4">
-            <span className="bg-primary-100 text-gray-300 text-2xl font-bold p-4 rounded-xl">
-              98
-            </span>
-            <div>
-              <h3 className="font-bold">Rank</h3>
-              <p className="text-gray-500">In top 30%</p>
-            </div>
-          </div>
-          <div className="bg-primary-100/10 rounded-xl p-4">
-            <div className="flex items-center gap-4 mb-4">
-              <span className="bg-primary-100 text-gray-300 text-2xl font-bold p-4 rounded-xl">
-                32
-              </span>
-              <div>
-                <h3 className="font-bold">Projects</h3>
-                <p className="text-gray-500">8 this month</p>
-              </div>
-            </div>
-            <div className="flex items-center gap-2 text-gray-500 text-sm">
-              <span className="bg-primary-100/20 py-1 px-4 rounded-full">
-                Mobile app
-              </span>
-              <span className="bg-primary-100/20 py-1 px-4 rounded-full">
-                Branding
-              </span>
-            </div>
-          </div>
-        </div>
-        {/* Card 3 */}
-        <div className="col-span-1 md:col-span-2 flex flex-col justify-between">
-          <h1 className="text-2xl font-bold mb-8">Your projects</h1>
-          <div className="bg-white p-8 rounded-xl shadow-2xl">
-            <div className="flex items-center gap-4 mb-8">
-              <img
-                src="https://img.freepik.com/foto-gratis/retrato-mujer-mayor-cerca_23-2149207185.jpg"
-                className="w-14 h-14 object-cover rounded-full"
-              />
-              <div>
-                <h3 className="font-bold">Logo design for Bakery</h3>
-                <p className="text-gray-500">1 day remaining</p>
-              </div>
-            </div>
-            <div className="flex items-center gap-4 mb-4">
-              <img
-                src="https://img.freepik.com/foto-gratis/retrato-mujer-mayor-cerca_23-2149207185.jpg"
-                className="w-14 h-14 object-cover rounded-full"
-              />
-              <div>
-                <h3 className="font-bold">Logo design for Bakery</h3>
-                <p className="text-gray-500">1 day remaining</p>
-              </div>
-            </div>
-            <div className="flex justify-end">
-              <a
-                href="#"
-                className="hover:text-primary-100 transition-colors hover:underline"
-              >
-                See all projects
-              </a>
-            </div>
-          </div>
-        </div>
+      <div className="max-w-4xl mx-auto flex flex-col md:flex-row items-center gap-8 p-4 pb-8 rounded-xl bg-gray-100">
+
+  <div>
+    <img
+      src="https://www.neuqueninforma.gob.ar/wp-content/uploads/2017/01/WEB-NO-CABEZA...EPET-20.jpg"
+      className="object-cover rounded-xl w-full md:w-auto md:max-w-sm -mt-20 drop-shadow-2xl object-center"
+    />
+  </div>
+
+  <div>
+    <div className="flex items-center gap-4">
+      <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="text-5xl text-indigo-500" height="2em" width="1em" xmlns="http://www.w3.org/2000/svg">
+        <g>
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path d="M4.583 17.321C3.553 16.227 3 15 3 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621.537-.278 1.24-.375 1.929-.311 1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5c-1.073 0-2.099-.49-2.748-1.179zm10 0C13.553 16.227 13 15 13 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621.537-.278 1.24-.375 1.929-.311 1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5c-1.073 0-2.099-.49-2.748-1.179z"></path>
+            </g>
+      </svg>
+      <h2 className="text-2xl font-bold text-gray-900">
+        Lorem ipsum dolor sit amet
+      </h2>
+      <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="text-5xl text-indigo-500" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+        <g>
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path d="M19.417 6.679C20.447 7.773 21 9 21 10.989c0 3.5-2.457 6.637-6.03 8.188l-.893-1.378c3.335-1.804 3.987-4.145 4.247-5.621-.537.278-1.24.375-1.929.311-1.804-.167-3.226-1.648-3.226-3.489a3.5 3.5 0 0 1 3.5-3.5c1.073 0 2.099.49 2.748 1.179zm-10 0C10.447 7.773 11 9 11 10.989c0 3.5-2.457 6.637-6.03 8.188l-.893-1.378c3.335-1.804 3.987-4.145 4.247-5.621-.537.278-1.24.375-1.929.311C4.591 12.322 3.17 10.841 3.17 9a3.5 3.5 0 0 1 3.5-3.5c1.073 0 2.099.49 2.748 1.179z"></path>
+        </g>
+      </svg>
+    </div>
+    <p className="my-4 text-gray-500">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
+      quibusdam ducimus libero ad tempora doloribus expedita laborum
+      saepe voluptas perferendis delectus assumenda rerum, culpa aperiam
+      dolorum, obcaecati corrupti aspernatur a.
+    </p>
+    <div className="mb-8">
+      <h4 className="text-lg text-indigo-500 font-bold">
+        Luana Selena Esteve
+      </h4>
+      <p>
+        <span className="font-medium">Gerente</span> en{" "}
+        <span className="font-medium">AIDE CORP.</span>
+      </p>
+    </div>
+    <div className="flex items-center gap-4">
+      <button className="border border-indigo-500 p-2 rounded-xl hover:bg-indigo-500 group transition-colors">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 text-indigo-500 group-hover:text-white"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+          />
+        </svg>
+      </button>
+      <button className="border border-indigo-500 p-2 rounded-xl hover:bg-indigo-500 group transition-colors">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 text-indigo-500 group-hover:text-white"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+          />
+        </svg>
+      </button>
+    </div>
+  </div>
+</div>
       </section>
+      <h1 className="text-2xl font-bold mb-8 text-center">Creadoras:</h1>
       {/* Section 2 */}
       <section className="grid grid-cols-1 md:grid-cols-2 mt-10 gap-8">
+        
         <div>
-          <h1 className="text-2xl font-bold mb-8">Recent invoices</h1>
+       
           <div className="bg-white p-8 rounded-xl shadow-2xl mb-8 flex flex-col gap-8">
             {/* Card 1 */}
             <div className="grid grid-cols-1 xl:grid-cols-4 items-center gap-4 mb-4">
@@ -103,13 +109,13 @@ export default function HomeScreen() {
                   className="w-14 h-14 object-cover rounded-xl"
                 />
                 <div>
-                  <h3 className="font-bold">Alexander Williams</h3>
-                  <p className="text-gray-500">JQ Holdings</p>
+                  <h3 className="font-bold">Abigail Salcedo</h3>
+                  <p className="text-gray-500 text-center ">No se baña PROFESIONAL</p>
                 </div>
               </div>
               <div>
-                <span className="bg-green-100 text-green-800 py-1 px-3 rounded-full font-medium">
-                  Paid
+              <span className="bg-red-100 text-red-800 py-1 px-3 rounded-full font-medium">
+                  Gana
                 </span>
               </div>
               <div>
@@ -124,13 +130,13 @@ export default function HomeScreen() {
                   className="w-14 h-14 object-cover rounded-xl"
                 />
                 <div>
-                  <h3 className="font-bold">Jhon Philips</h3>
-                  <p className="text-gray-500">Inchor Techs</p>
+                  <h3 className="font-bold">Nicole Aramayo</h3>
+                  <p className="text-gray-500 text-center">Fumadora de cigarros PROFESIONAL</p>
                 </div>
               </div>
               <div>
                 <span className="bg-red-100 text-red-800 py-1 px-3 rounded-full font-medium">
-                  Late
+                  Gana
                 </span>
               </div>
               <div>
@@ -143,18 +149,16 @@ export default function HomeScreen() {
               <RiHashtag className="text-4xl -rotate-12" />
             </div>
             <div>
-              <h5 className="font-bold text-white">Engage with clients</h5>
-              <h5>Join slack channel</h5>
+              <h5 className="font-bold text-white">Excelencia</h5>
             </div>
             <div className="w-full xl:w-auto">
               <button className="bg-primary-100 py-2 px-6 rounded-xl text-white w-full">
-                Join now
+              APROBAR
               </button>
             </div>
           </div>
         </div>
         <div>
-          <h1 className="text-2xl font-bold mb-8">Recommended project</h1>
           <div className="bg-white p-8 rounded-xl shadow-2xl mb-8 flex flex-col gap-8">
             <div className="flex flex-col xl:flex-row xl:items-center justify-between gap-4">
               <div className="flex items-center gap-4">
@@ -163,23 +167,22 @@ export default function HomeScreen() {
                   className="w-14 h-14 object-cover rounded-full"
                 />
                 <div>
-                  <h3 className="font-bold">Thomas Martin</h3>
-                  <p className="text-gray-500">Updated 10m ago</p>
+                  <h3 className="font-bold">Luana Esteve</h3>
+                  <p className="text-gray-500">Se come los mocos</p>
                 </div>
               </div>
               <div>
                 <span className="bg-primary-100 py-2 px-4 rounded-full text-white">
-                  Design
+                  Diseñadora
                 </span>
               </div>
             </div>
             <div>
               <h5 className="text-lg font-bold">
-                Need a designer to form branding essentials for my business.
+
               </h5>
               <p className="text-gray-500">
-                Looking for a talented brand designer to create all the
-                branding materials my new startup.
+                Clara necesidad de un mate.
               </p>
             </div>
             <div className="bg-primary-100/10 flex flex-col md:flex-row items-center justify-between gap-4 py-8 px-4 rounded-lg">
@@ -189,9 +192,7 @@ export default function HomeScreen() {
                 <span className="text-sm text-gray-500">/ month</span>
               </div>
               <div>
-                <span className="border border-primary-100 text-primary-100 py-2 px-4 rounded-full">
-                  Full time
-                </span>
+              
               </div>
             </div>
           </div>
