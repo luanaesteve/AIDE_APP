286be2f80132d5eff8d0a84cc9251d14a5fcd540
luana <luanaesteve73@gmail.com>
2022-11-14 19:04:20 -0300

    jaja

--- src/UI/Components/Header.js
+++ src/UI/Components/Header.js
@@ -10,10 +10,10 @@ const Header = () => {
       </h1>
       <form className="w-full md:w-auto">
         <div className="relative">
-          <RiSearch2Line className="absolute top-1/2 -translate-y-1/2 left-2" />
+          <RiSearch2Line className="absolute top-1/2 -translate-y-1/2 left-2 fill-white" />
           <input
             type="text"
-            className="bg-gray-200 outline-none py-2 pl-8 pr-4 rounded-xl w-full md:w-auto"
+            className="bg-purple-700 outline-none py-2 pl-8 pr-4 rounded-xl w-full md:w-auto placeholder-white"
             placeholder="Buscar contenido"
           />
         </div>
