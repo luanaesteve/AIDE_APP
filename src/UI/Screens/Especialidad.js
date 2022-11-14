8bd39307c97f7e1df8ba51164aa2f5261d4fe57d
nicol21aramayo <nicol.aramayo.epet20@gmail.com>
2022-11-14 17:14:52 -0300

    jajajaj

--- src/UI/Screens/Especialidad.js
+++ src/UI/Screens/Especialidad.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Sidebar from "../Components/SideBar";
+import Header from "../Components/Header";
 
  
 export default function Especialidad() {
    return (
-
-    <h1>PUTAAAAAAAAAAAAAAa</h1>
+    <div className="grid lg:grid-cols-4 xl:grid-cols-6 min-h-screen">
+    <Sidebar />
+    <main className="lg:col-span-3 xl:col-span-5 bg-gray-100 p-10 h-[100vh] overflow-y-scroll ">
+      <Header />
+      </main>
+      </div>
    )
   
 
