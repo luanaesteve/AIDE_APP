3f1908bed5aa1d7d42f77789f56e08c0bc94843e
luana <luanaesteve73@gmail.com>
2022-11-14 18:56:57 -0300

    djasdja

--- src/UI/Components/SideBar.js
+++ src/UI/Components/SideBar.js
@@ -46,7 +46,9 @@ const Sidebar = () => {
               href="#"
               className="flex items-center gap-4 text-white py-2 px-4 rounded-xl hover:bg-purple-800 transition-colors"
             >
-              <RiHome3Line /> Inicio
+              <RiHome3Line /> <Link to="/home">
+          Inicio
+        </Link>
             </a>
             <a
               href="#"
@@ -60,13 +62,17 @@ const Sidebar = () => {
               href="#"
               className="flex items-center gap-4 text-white py-2 px-4 rounded-xl hover:bg-purple-800 transition-colors"
             >
-              <RiCalendarCheckFill /> Calendario
+              <RiCalendarCheckFill /> <Link to="/calendario">
+          Calendario
+        </Link>
             </a>
             <a
               href="#"
               className="flex items-center gap-4 text-white py-2 px-4 rounded-xl hover:bg-purple-800 transition-colors"
             >
-              <RiFingerprintFill /> Ajustes
+              <RiFingerprintFill /> <Link to="/asesoria">
+          Asesoria
+        </Link>
             </a>
         
           </nav>
