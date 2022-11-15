8ebdab89b967aedc155fb92fda5fe6f4a6977b12
luana <luanaesteve73@gmail.com>
2022-11-15 02:08:31 -0300

    colores

--- src/UI/Screens/ProtectedRoute.js
+++ src/UI/Screens/ProtectedRoute.js
@@ -4,7 +4,7 @@ import { useAuth } from "../Context/AuthContext";
 export function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
 
-  if (loading) return <h1>CARGANDO</h1>;
+  if (loading) return <h1 className="text-center p-20 gap-10 font-bold ">CARGANDO</h1>;
 
   if (!user) return <Navigate to="/" />;
 
